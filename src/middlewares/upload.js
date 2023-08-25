165bfba4b8dd54516ed63e8edf0e62d485526b6c
Lula <ludmilamiceli@hotmail.com>
2023-08-25 00:44:09 -0300

    Extra no funcional

---
+++ src/middlewares/upload.js
@@ -0,0 +1,20 @@
+const multer = require('multer');
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/public/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null,`${Date.now()}_products_${path.extname(file.originalname)}`)
+
+    }
+})
+const upload = multer({ 
+    storage 
+})
+
+module.exports = {
+    upload   //variable donde se guarda la info
+}
